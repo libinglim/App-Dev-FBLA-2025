525ee4daf90ce5de834abad44476a03c1e457ade
Logan Lin <logan.lin.yc@gmail.com>
2025-01-12 14:14:41 -0600

    user data and dummy users

---
+++ app/lib/userData.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class UserData {
+  String name = "";
+  String teacher = "";
+  List<String> robots = [];
+  List<String> accessories = [];
+  int questsCompleted = 0;
+  int questionsAnswered = 0;
+  int roboOutletWins = 0;
+
+  UserData(String _name, String _teacher, _robots) {
+    name = _name;
+    teacher = _teacher;
+    robots = [_robots];
+  }
+
+  getName() {
+    return name;
+  }
+
+  setName(String _name) {
+    name = _name;
+  }
+
+  getTeacher() {
+    return teacher;
+  }
+
+  setTeacher(String _teacher) {
+    teacher = _teacher;
+  }
+
+  addRobot(String _robot) {
+    robots.add(_robot);
+  }
+
+  addAccessories(String _accessories) {
+    accessories.add(_accessories);
+  }
+
+  addQuests() {
+    questsCompleted++;
+  }
+
+  addAnswered() {
+    questionsAnswered++;
+  }
+
+  addWins() {
+    roboOutletWins++;
+  }
+}
---
+++ app/lib/users.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:app/userData.dart';
+
+List<UserData> users = [
+  UserData('Billy', 'Mrs.Depaul', 'images/RadRobot.png'),
+  UserData('John', 'Mr.Mitchem', 'images/OvalRobot.png'),
+  UserData('Brady', 'Mr.Campion', 'images/SquareRobot.png'),
+  UserData('Evan', 'Mr.Pinta', 'images/robot.png'),
+  UserData('Lily', 'Mr.Mitchem', 'images/FemaleRobot.png'),
+  UserData('Mina', 'Mrs.Howard', 'images/WinkingRobot.png'),
+  UserData('Emma', 'Mr.Pinta', 'images/OvalRobot.png'),
+];
+
+UserData billy = users[0];
+UserData john = users[1];
+UserData brady = users[2];
+UserData evan = users[3];
+UserData lily = users[4];
+UserData mina = users[5];
+UserData emma = users[6];
